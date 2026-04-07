ef302b7e3c43b519134dd11297a5a7e22cbf7435
Ethan Arrowood <ethan@arrowood.dev>
2026-04-07 15:05:13 -0600

    add invalidate example with custom resource implementation

--- resources.js
+++ resources.js
@@ -7,3 +7,13 @@ class JokeAPI extends Resource {
 }
 
 tables.JokeCache.sourcedFrom(JokeAPI);
+
+export class JokeCache extends tables.JokeCache {
+	static async post(target, data) {
+		const body = await data;
+		if (body?.action === 'invalidate') {
+			this.invalidate(target);
+			return { status: 200, data: { message: 'invalidated' } };
+		}
+	}
+}
