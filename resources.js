73687f73b77fb4c455b8ffe51e871450042d0f89
Ethan Arrowood <ethan@arrowood.dev>
2026-04-07 15:03:54 -0600

    add JokeCache example

--- resources.js
+++ resources.js
@@ -1,1 +1,9 @@
-// Custom Resources
+class JokeAPI extends Resource {
+	async get() {
+		const id = this.getId();
+		const response = await fetch(`https://official-joke-api.appspot.com/jokes/${id}`);
+		return response.json();
+	}
+}
+
+tables.JokeCache.sourcedFrom(JokeAPI);
